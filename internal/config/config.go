a72ea005d891dc779cbaaddc7f39230068e882d4
Vortikcallace <vortikcallace@gmail.com>
2025-10-07 19:16:05 +0300

    config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,30 @@
 package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBHost     string `mapstructure:"DB_HOST"`
+	DBPort     string `mapstructure:"DB_PORT"`
+	DBUser     string `mapstructure:"DB_USER"`
+	DBPassword string `mapstructure:"DB_PASSWORD"`
+	DBName     string `mapstructure:"DB_NAME"`
+	ServerPort string `mapstructure:"SERVER_PORT"`
+}
+
+func LoadConfig() (*Config, error) {
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	var config Config
+	if err := viper.Unmarshal(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
