ab8d20075eeeeff1f5ec74936481a8949fa5de66
Vortikcallace <vortikcallace@gmail.com>
2025-10-08 17:27:22 +0300

    database

--- internal/database/database.go
+++ internal/database/database.go
@@ -1,1 +1,30 @@
 package database
+
+import (
+	"fmt"
+	"test-service-for-pick-up-points/internal/config"
+	"test-service-for-pick-up-points/internal/models"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type Database struct {
+	DB *gorm.DB
+}
+
+func NewDatabase(cfg *config.Config) (*Database, error) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBPort)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.AutoMigrate(&models.User{}, &models.Product{}, &models.Order{}); err != nil {
+		return nil, err
+	}
+
+	return &Database{DB: db}, nil
+}
--- internal/services/service.go
+++ internal/services/service.go
@@ -1,1 +1,7 @@
 package services
+
+import "test-service-for-pick-up-points/internal/database"
+
+type UserService struct {
+	userRepo *database.UserRepository
+}
