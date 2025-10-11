7fdefc2843cf38bb6730b93021d47c911436b29a
Vortikcallace <vortikcallace@gmail.com>
2025-10-11 20:13:48 +0300

    user route

--- internal/transport/routes/routes.go
+++ internal/transport/routes/routes.go
@@ -1,1 +1,30 @@
 package routes
+
+import (
+	"test-service-for-pick-up-points/internal/database"
+	"test-service-for-pick-up-points/internal/services"
+	"test-service-for-pick-up-points/internal/transport/handlers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(db *database.Database) *gin.Engine {
+	router := gin.Default()
+
+	userRepo := database.NewUserRepository(db.DB)
+
+	userService := services.NewUserService(userRepo)
+
+	userHandler := handlers.NewUserHandler(userService)
+
+	api := router.Group("/api/v1")
+	{
+		users := api.Group("/users")
+		{
+			users.POST("/", userHandler.CreateUser)
+			users.GET("/:id", userHandler.GetUser)
+		}
+	}
+
+	return router
+}
