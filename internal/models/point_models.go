da01113a05359591d9cce78277fb4a636697a487
Vortikcallace <vortikcallace@gmail.com>
2025-10-11 21:19:35 +0300

    point model update

--- internal/models/point_models.go
+++ internal/models/point_models.go
@@ -8,8 +8,8 @@ import (
 
 type Point struct {
 	ID            uint           `gorm:"primaryKey" json:"id"`
-	ProductID     uint           `gorm:"not null" json:"product_id"`
 	Open_or_Close bool           `gorm:"default:false" json:"readiness"`
+	Address       string         `gorm:"type:varchar(255);not null" json:"address"`
 	CreatedAt     time.Time      `json:"created_at"`
 	UpdatedAt     time.Time      `json:"updated_at"`
 	DeletedAt     gorm.DeletedAt `gorm:"index" json:"-"`
