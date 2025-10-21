ffa5bc6597a3ab89c1295cd16181ddce3aa3405c
Vortikcallace <vortikcallace@gmail.com>
2025-10-22 01:59:38 +0300

    edit routes

--- internal/transport/routes/routes.go
+++ internal/transport/routes/routes.go
@@ -12,10 +12,19 @@ func SetupRoutes(db *database.Database) *gin.Engine {
 	router := gin.Default()
 
 	userRepo := database.NewUserRepository(db.DB)
+	orderRepo := database.NewOrderRepository(db.DB)
+	productRepo := database.NewProductRepository(db.DB)
+	pointRepo := database.NewPointRepository(db.DB)
 
 	userService := services.NewUserService(userRepo)
+	orderService := services.NewOrderService(orderRepo, userRepo, productRepo)
+	productService := services.NewProductService(productRepo)
+	pointService := services.NewPointService(pointRepo)
 
 	userHandler := handlers.NewUserHandler(userService)
+	orderHandler := handlers.NewOrderHandler(orderService)
+	productHandler := handlers.NewProductHandler(productService)
+	pointHandler := handlers.NewPointHandler(pointService)
 
 	api := router.Group("/api/v1")
 	{
@@ -23,6 +32,27 @@ func SetupRoutes(db *database.Database) *gin.Engine {
 		{
 			users.POST("/", userHandler.CreateUser)
 			users.GET("/:id", userHandler.GetUser)
+		}
+
+		orders := api.Group("/orders")
+		{
+			orders.POST("/", orderHandler.CreateOrder)
+			orders.GET("/user/:user_id", orderHandler.GetUserOrders)
+			orders.PATCH("/:id/readiness", orderHandler.UpdateOrderReadiness)
+		}
+
+		products := api.Group("/products")
+		{
+			products.POST("/", productHandler.CreateProduct)
+			products.GET("/", productHandler.GetProducts)
+			products.GET("/:id", productHandler.GetProduct)
+		}
+
+		points := api.Group("/points")
+		{
+			points.POST("/", pointHandler.CreatePoint)
+			points.GET("/", pointHandler.GetPoints)
+			points.GET("/:id", pointHandler.GetPoint)
 		}
 	}
 
