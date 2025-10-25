0e06302b38d659e439cd57923c134ed403e36a6b
Vortikcallace <vortikcallace@gmail.com>
2025-10-26 02:02:10 +0300

    redis env

--- internal/redis/redis.go
+++ internal/redis/redis.go
@@ -3,6 +3,7 @@ package redis
 import (
 	"context"
 	"log"
+	"os"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -12,16 +13,13 @@ var Rdb *redis.Client
 
 func InitReddis() {
 	Rdb = redis.NewClient(&redis.Options{
-		Addr:     "redis-cache:6379",
-		Password: "2242",
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Password: os.Getenv("REDIS_PASSWORD"),
 		DB:       0,
 	})
 
 	_, err := Rdb.Ping(Ctx).Result()
-
 	if err != nil {
-		log.Println("Error conect redis", err)
-		return
+		log.Fatalf("Error connecting to Redis: %v", err)
 	}
-
 }
