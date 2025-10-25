fdf2ce7e8c170b0307ff4608a941e64d584c6763
Vortikcallace <vortikcallace@gmail.com>
2025-10-26 02:08:58 +0300

    edit redis

--- internal/redis/redis.go
+++ internal/redis/redis.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"os"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -22,4 +23,22 @@ func InitReddis() {
 	if err != nil {
 		log.Fatalf("Error connecting to Redis: %v", err)
 	}
+}
+
+func SetValue(key string, value interface{}) error {
+	ctx, cancel := context.WithTimeout(Ctx, 5*time.Second)
+	defer cancel()
+	return Rdb.Set(ctx, key, value, 0).Err()
+}
+
+func GetValue(key string) (string, error) {
+	ctx, cancel := context.WithTimeout(Ctx, 5*time.Second)
+	defer cancel()
+	val, err := Rdb.Get(ctx, key).Result()
+	if err == redis.Nil {
+		return "", nil
+	} else if err != nil {
+		return "", err
+	}
+	return val, nil
 }
