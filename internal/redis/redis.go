f9b295aac39243ec9886ece9f1d9947e5a2ea6b9
Vortikcallace <vortikcallace@gmail.com>
2025-10-26 01:26:59 +0300

    init redis

--- internal/redis/redis.go
+++ internal/redis/redis.go
@@ -1,1 +1,27 @@
 package redis
+
+import (
+	"context"
+	"log"
+
+	"github.com/redis/go-redis/v9"
+)
+
+var Ctx = context.Background()
+var Rdb *redis.Client
+
+func InitReddis() {
+	Rdb = redis.NewClient(&redis.Options{
+		Addr:     "redis-cache:6379",
+		Password: "2242",
+		DB:       0,
+	})
+
+	_, err := Rdb.Ping(Ctx).Result()
+
+	if err != nil {
+		log.Println("Error conect redis", err)
+		return
+	}
+
+}
